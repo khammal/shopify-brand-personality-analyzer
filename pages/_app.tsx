ff622d8e625910baf5a56e0d3d268a60437250c3
khammal <karam.hammal@gmail.com>
2025-08-19 15:55:09 -0400

    Update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/sparkles.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/sparkles.svg" />
       </Head>
       <div className="font-sans antialiased">
         <Component {...pageProps} />
